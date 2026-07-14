10dfe8910a15db110ab9a966f39abf0a5a81d603
mikan20020 <perc.mikan@gmail.com>
2026-07-14 22:28:15 +0900

    Delete js directory

--- js/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase Consoleの「プロジェクトの設定」→「マイアプリ」から取得した値を貼り付けてください。
-window.FIREBASE_CONFIG = {
-  apiKey: "ここにapiKey",
-  authDomain: "ここにauthDomain",
-  projectId: "ここにprojectId",
-  storageBucket: "ここにstorageBucket",
-  messagingSenderId: "ここにmessagingSenderId",
-  appId: "ここにappId"
-};
